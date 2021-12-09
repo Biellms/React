e2b3a59a74a728333e13446d32c315ebaf242145
Biellms <biell.mendes8@gmail.com>
2021-12-09 15:41:25 -0300

    Add post, put postagem

--- Blog Pessoal/src/paginas/home/Home.tsx
+++ Blog Pessoal/src/paginas/home/Home.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography, Box, Grid, Button } from '@material-ui/core';
 import './Home.css';
 import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
+import ModalPostagem from '../../components/postagens/modealPostagem/ModalPostagem';
+import { useHistory } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
 
 function Home() {
+
+  let history = useHistory();
+  const [token, setToken] = useLocalStorage('token');
+
+  useEffect(() => {
+      if (token == "") {
+          alert("Você precisa estar logado")
+          history.push("/login")
+      }
+  }, [token])
 
   return (
     <div >
@@ -15,6 +28,7 @@ function Home() {
           </Box>
           <Box display="flex" justifyContent="center">
             <Box marginRight={1}>
+              <ModalPostagem />
             </Box>
             <Button variant="outlined" className='botao'>Ver Postagens</Button>
           </Box>
